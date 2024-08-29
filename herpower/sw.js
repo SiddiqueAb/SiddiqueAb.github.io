d20ea317dd85c611f59d552a6c2be2f8b2978971
AB SIDDIQUE <31104573+SiddiqueAb@users.noreply.github.com>
2024-08-29 23:26:18 +0600

    Working for herpower page

---
+++ herpower/sw.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'ab-siddique-cache-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    'https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600&family=Roboto:wght@300;400&display=swap',
+    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
