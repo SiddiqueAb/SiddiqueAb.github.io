4255759aaaf67f950a6cb24924519fea7c5ff2cc
AB SIDDIQUE <31104573+SiddiqueAb@users.noreply.github.com>
2024-10-07 17:12:59 +0600

    Class 65

---
+++ herpower/65-class-07-10-2024/script.js
@@ -0,0 +1,25 @@
+// let button = document.getElementById("button");
+
+// document.getElementById("parent_div").addEventListener("click", function () {
+//   alert("You clicked Parent Div");
+// });
+
+// document.getElementById("button").addEventListener("click", function () {
+//   alert("You clicked Button");
+// });
+
+// document.getElementById("grand_parent").addEventListener("click", function () {
+//   alert("You clicked Grand Parent");
+// });
+// document.getElementById("list").addEventListener("click", function (e) {
+//   e.target.style.backgroundColor = "red";
+// });
+
+// import message from "./person.js";
+
+// document.getElementById("id02").innerHTML = message();
+// document.getElementById("id02").innerHTML =
+//   document.getElementById("id01").firstChild.nodeValue;
+// console.log(document.getElementById("id01").firstChild);
+
+
